6bcdc8147ce7f2f7893a2339ba2d6851380f12c7
Opilane TTHK <opilane@tthk.ee>
2022-03-04 09:53:31 +0200

    IkeaDB

--- IkeaDB/Form1.Designer.cs
+++ IkeaDB/Form1.Designer.cs
@@ -73,6 +73,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(1053, 750);
             this.Controls.Add(this.listSeries);
             this.Controls.Add(this.label2);
